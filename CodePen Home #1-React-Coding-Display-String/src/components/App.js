559e2e1d0c0d8f9630db64eccb5019d5fb2bf141
Maria Luisa Lufrano <marialuisa.lufrano@icloud.com>
2022-09-30 12:38:36 +0200

    Added first exercise (display a string

---
+++ CodePen Home #1-React-Coding-Display-String/src/components/App.js
@@ -0,0 +1,11 @@
+import '../styles/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <p>JSX is cool!</p>
+    </div>
+  );
+}
+
+export default App;
--- README.md
+++
@@ -1,2 +0,0 @@
-# 10-react-coding-exercises
-This is a repo with 10 react exercise
